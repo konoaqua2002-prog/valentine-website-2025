73d8eeb2cc3d2c501ae7702d9593eedfde6ce924
konoaqua2002-prog <konoaqua2002@gmail.com>
2026-02-11 21:40:44 +0800

    Update celebration message in config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Iloveyouuuu langga mwamwamwamwaaaaaaaaa!",
+        message: "Iloveyouuuu langga mwamwamwa!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
